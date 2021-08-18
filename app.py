e2a0d3a283dbdb7e53ccbd9cb3b1f99fe4316fb9
Matthew Laux <matthew.laux@gmail.com>
2021-08-17 22:39:42 -0500

    tweak video settings

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 import os
 import binascii
 
+
+# cinepak also works and looks a little better, slightly bigger file size
+# also slooooow to compress
+VIDEO_CODEC = "svq1"
+# pcm_s16be, pcm_u8, pcm_mulaw, adpcm_ms also work
+AUDIO_CODEC = "adpcm_ms"
+
 app = Flask(__name__)
 
 @app.route("/video/<id>")
