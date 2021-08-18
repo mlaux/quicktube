bced817b47387ac4f9205d06d57ebc8fecefac01
Matthew Laux <matthew.laux@gmail.com>
2021-08-17 19:36:30 -0500

    temporary cache busting

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+import os
+import binascii
 
 app = Flask(__name__)
 
 @app.route("/video/<id>")
 def get_video(id):
-    return render_template('video.html', video_id=id)
+    nocache_id = binascii.b2a_hex(os.urandom(4)).decode('utf-8')
+    return render_template('video.html', video_id=id, nocache_id=nocache_id)
